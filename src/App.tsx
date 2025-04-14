668edb63069d93b7eb6f22b79ce18cae128011ed
michael <roringmichael@gmail.com>
2025-04-14 13:09:10 +0700

    feat: add accordions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Header from "./components/header";
+import HowToAccordion from "./components/HowToAccordion";
+import { HeroUIProvider } from "@heroui/react";
 
 function App() {
   return (
     <>
-      <div className="bg-slate-100 py-20">
-        <Header />
-      </div>
+      <HeroUIProvider>
+        <div className="flex flex-col min-h-screen p-6 bg-blue-50">
+          <div className="bg-white rounded-2xl mx-auto w-11/12">
+            <Header />
+            <HowToAccordion />
+          </div>
+        </div>
+      </HeroUIProvider>
     </>
   );
 }
