3da240b09bdc29c3ec37f1721c517255acf0ed2f
Agiff <ghifarimohammadrahadian@gmail.com>
2025-04-19 22:27:22 +0700

    feat: update Vite configuration with build options and dependency exclusions

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,17 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  build: {
+    target: 'esnext',
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        manualChunks: () => 'index.js',
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 })
