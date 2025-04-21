b9fca49a363a7b73b57d744c95009ade5a89b4dc
Agiff <ghifarimohammadrahadian@gmail.com>
2025-04-21 11:16:56 +0700

    feat: add vite-plugin-css-injected-by-js to dependencies and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss(), flowbiteReact(), cssInjectedByJsPlugin()],
   build: {
     target: 'esnext',
     cssCodeSplit: false,
     rollupOptions: {
       output: {
         entryFileNames: 'index.js',
-        manualChunks: () => 'index.js',
+        manualChunks: undefined,
       },
     },
   },
