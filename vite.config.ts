ee6fcc3329c7f974e565fd18f8de9bc8e0969108
Agiff <ghifarimohammadrahadian@gmail.com>
2025-04-21 11:33:53 +0700

    refactor: clean up Vite configuration by removing unused plugins and fixing manualChunks definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact(), cssInjectedByJsPlugin()],
+  plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext',
     cssCodeSplit: false,
     rollupOptions: {
       output: {
         entryFileNames: 'index.js',
-        manualChunks: undefined,
+        manualChunks: () => 'index.js',
       },
     },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
