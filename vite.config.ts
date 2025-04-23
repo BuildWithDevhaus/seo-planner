d5373b2fbb131cdfe6a95f2ea4f6d9ae2ae8a454
michael <roringmichael@gmail.com>
2025-04-23 10:12:11 +0700

    fix: container width

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": "/src", // This is relative to the project root
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
